b9f636b0c72e187fa9c6b57a660b381bc7d084e1
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-10-29 21:56:56 +0100

    Add extra long factorials

---
+++ Medium/Medium/extra_long_factorials_medium.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+
+namespace  _1
+{
+	std::vector<int> muptiply_big(std::vector<int> mul, int m)
+	{
+		std::vector<int> to_return;
+		int carry = 0;
+		for (auto it = mul.rbegin(); it != mul.rend(); ++it)
+		{
+			int result = *it*m + carry;
+			to_return.push_back(result % 10);
+			carry = result / 10;
+		}
+		while (carry)
+		{
+			to_return.push_back(carry % 10);
+			carry /= 10;
+		}
+		std::reverse(to_return.begin(), to_return.end());
+		return  to_return;
+	}
+}
