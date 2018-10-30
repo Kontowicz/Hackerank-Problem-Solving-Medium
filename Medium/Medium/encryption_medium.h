72c7d33a91a37b13b2f38b316550d37251529ffc
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-10-30 22:46:17 +0100

    Add enryption

---
+++ Medium/Medium/encryption_medium.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <algorithm>
+
+namespace _2
+{
+	std::string encryption(std::string s) {
+		std::string::iterator end_pos = std::remove(s.begin(), s.end(), ' ');
+		s.erase(end_pos, s.end());
+		int str_len_down = floor(sqrt(s.length()));
+		int str_len_up = ceil(sqrt(s.length()));
+
+		std::string to_return = "";
+		if (str_len_down * str_len_up < s.length()) str_len_down = str_len_up;
+		for (int i = 0; i < str_len_up; i++)
+		{
+			for (int j = i; j < s.length(); j += str_len_up)
+			{
+				to_return += s[j];
+			}
+			to_return += " ";
+		}
+		return to_return;
+	}
+}
