2d08a52759f4673b959e7b1fd6c233df5fe091a1
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-11-14 20:49:48 +0100

    Refactor

--- Medium/Medium/the_grid_search_medium.h
+++ Medium/Medium/the_grid_search_medium.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <iterator>
 
 namespace _5
 {
@@ -20,5 +21,28 @@ namespace _5
 			}
 
 		return "NO";
+	}
+
+	void run()
+	{
+		std::cout << "The grid search.\nEnter grid and patterns size:\n";
+		int grid_size, patterns_size;
+		std::vector<std::string> grid;
+		std::vector<std::string> pattern;
+		std::string tmp;
+		for (int i = 0; i < grid_size; ++i)
+		{
+			std::cin >> tmp;
+			grid.push_back(tmp);
+		}
+			
+		for (int i = 0; i < patterns_size; ++i)
+		{
+			std::cin >> tmp;
+			pattern.push_back(tmp);
+		}
+
+		std::cout << "\nResult:";
+		std::cout << gridSearch(grid, pattern);
 	}
 }
